2599a498d925c66ac47f6eb32989ba51a1bf23fe
namkh0510 <nam_0510@naver.com>
2021-09-10 18:02:10 +0900

    9월10일

--- nodejs/promis.js
+++ nodejs/promis.js
@@ -1,12 +1,12 @@
-const get = url =>{
+const get = url => {
     const xhr = new XMLHttpRequest();
-    xhr.open('Get',url);
+    xhr.open('Get', url);
     xhr.send();
 
-    xhr.onload = ()=>{
-        if(xhr.status === 200){
+    xhr.onload = () => {
+        if (xhr.status === 200) {
             console.log(JSON.parse(xhr.response));
-        }else{
+        } else {
             console.error(`${xhr.status} ${xhr.statusText}`);
         }
     };
@@ -14,3 +14,26 @@ const get = url =>{
 
 const response = get('https://jsonplaceholder.typicode.com/posts/1');
 console.log(response);
+
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+const promiseGet = url => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.send();
+
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(JSON.parse(xhr.response));
+            } else {
+                reject(new Error(xhr.status));
+            }
+        };
+    });
+}
+
+promiseGet(url);
+
+
+new Promise(resolve => resolve('fulfilled'))
+    .then(v=>console.log(v),e=>console.error(e));
